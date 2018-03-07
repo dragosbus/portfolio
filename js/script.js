2ccc4fbc836de06b4871e044bdd30143f69c80a2
dragosbus <dragosbusuioc94@gmail.com>
2018-03-07 22:02:04 +0100

    Removed js from gitignore

---
+++ js/script.js
@@ -0,0 +1,36 @@
+(function () {
+
+    var hamburger = (function () {
+        var btn = document.querySelector('.btn__hamburger');
+        var nav = document.querySelector('.nav');
+
+        var toggle = function () {
+            nav.classList.toggle('toggle-on');
+            console.log('on');
+        }
+
+        return {
+            btn,
+            toggle
+        };
+    }());
+
+    hamburger.btn.addEventListener('click', hamburger.toggle);
+
+    var scrollNav = (function (window) {
+        var nav = document.querySelector('.header__top');
+        var ul = document.querySelector('.nav');
+
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset >= 520) {
+                nav.classList.add('top-black');
+                ul.style.backgroundColor = 'rgb(34, 33, 33)';
+            } else {
+                nav.classList.remove('top-black');
+                ul.style.backgroundColor = 'transparent';
+            }
+        });
+
+    }(window));
+
+}());
