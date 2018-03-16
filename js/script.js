3b1eb3d8d770269436af4f854b1736cf9fe52d66
dragosbus <dragosbusuioc94@gmail.com>
2018-03-16 17:47:28 +0100

    Added smooth scroll and changed the background of the header

--- js/script.js
+++ js/script.js
@@ -24,13 +24,52 @@
         window.addEventListener('scroll', () => {
             if (window.pageYOffset >= 420) {
                 headerTop.classList.add('top-black');
-                ul.style.backgroundColor = 'rgb(34, 33, 33)';
+                ul.style.backgroundColor = 'rgb(26, 32, 47)';
             } else {
                 headerTop.classList.remove('top-black');
                 ul.style.backgroundColor = 'transparent';
             }
         });
 
+        //hide navigation when a link is clicked
+        ul.addEventListener('click', e => {
+            if (e.target.tagName = 'A') {
+                ul.classList.toggle('toggle-on');
+           } 
+        });
+
     }(window));
+
+    var smothScroll = (function () {
+        var sections = Array.from(document.querySelectorAll('section'));
+        var sectionProps = sections.map(function (section) {
+            return {
+                id: section.attributes[0].value,
+                yPos: section.getBoundingClientRect().top - (document.querySelector('nav').offsetHeight - 1)
+            }
+        })
+
+        var anchorLinks = Array.from(document.querySelectorAll('a'));
+
+        anchorLinks.forEach(function (link) {
+
+            link.addEventListener('click', function () {
+                console.log(link);
+                var linkNoHash = link.getAttribute('data-scroll-to').replace('#', '');
+                for (var i = 0; i < sectionProps.length; i++) {
+
+                    if (linkNoHash === sectionProps[i].id) {
+                        var yPos = sectionProps[i].yPos
+                        window.scrollTo({
+                            top: yPos,
+                            left: 0,
+                            behavior: 'smooth'
+                        });
+                    }
+                }
+            })
+        })
+
+    }());
 
 }());
