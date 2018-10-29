ea30bd0335d556be6c18da1d4fdab184d51e2806
dragosbus <dragosbusuioc94@gmail.com>
2018-10-29 14:39:29 +0100

    added animation for the header when the hamburger button is clicked

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ document.querySelector('.btn-hamburger').addEventListener('click', e=>{
   let right = document.querySelector('.navigation').style.right;
   if(right === '0px') {
     document.querySelector('.navigation').style.right = '-10000px';
+    document.querySelector('.btn-hamburger').style.transform = 'translate3d(0, 0, 0)';
+    document.querySelector('.logo').style.transform = 'translate3d(0, 0, 0)';
+    document.querySelector('.intro').style.transform = 'translate3d(0, 0, 0)';
   } else {
     document.querySelector('.navigation').style.right = 0;
+    document.querySelector('.btn-hamburger').style.transform = 'translate3d(-200px, 0, 0)';
+    document.querySelector('.logo').style.transform = 'translate3d(-100px, 0, 0)';
+    document.querySelector('.intro').style.transform = 'translate3d(-100px, 0, 0)';
   }
 });
